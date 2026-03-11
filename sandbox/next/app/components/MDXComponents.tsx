525880ed46c54d1e69279f2b9a9b64bb043543fb
Côme Bruneteau <comebruneteaupro@gmail.com>
2026-03-11 14:43:57 +0100

    chore: remove sandbox from core repository

--- sandbox/next/app/components/MDXComponents.tsx
+++
@@ -1,9 +0,0 @@
-import { DataChart } from "./DataChart";
-import { Note } from "./Note";
-import { Speaker } from "./Speaker";
-
-export const MDX_COMPONENTS = {
-  DataChart,
-  Note,
-  Speaker
-};
