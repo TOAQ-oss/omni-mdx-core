675a8cc24ab958ce8a4bf99bb540e759ca3af3ac
Côme Bruneteau <comebruneteaupro@gmail.com>
2026-03-22 18:52:37 +0100

    fix(mdx-next): add fallback to get error message in parseMdxClient function

--- packages/mdx-next/src/parse.client.ts
+++ packages/mdx-next/src/parse.client.ts
@@ -36,6 +36,8 @@ export async function parseMdxClient(mdx: string | Uint8Array): Promise<AstNode[
     const decoder = new MdxBinaryDecoder(binaryAst);
     return decoder.decode();
   } catch (err) {
-    throw new Error("[toaq-oss/omni-mdx] Syntax error in MDX: " + (err as Error).message);
+    const errorMessage = err instanceof Error ? err.message : String(err);
+    
+    throw new Error(`Syntax error in MDX: ${errorMessage}`);
   }
 }
